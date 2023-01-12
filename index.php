3856d1333feb6b3e0b5e9ae8f46a1a046383b932
contato12 <contato12araujo@gmail.com>
2023-01-11 23:30:58 -0300

    Header, body, footer and CSS.

--- index.php
+++ index.php
@@ -13,18 +13,59 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MovieStar</title>
-    <link rel="short icon" href="img/moviestar.ico">
+    <link rel="short icon" href="<?=$BASE_URL?>img/moviestar.ico">
     <!--Bootstrap-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.3/css/bootstrap.css" integrity="sha512-bR79Bg78Wmn33N5nvkEyg66hNg+xF/Q8NA8YABbj+4sBngYhv9P8eum19hdjYcY7vXk/vRkhM3v/ZndtgEXRWw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!--Font Awesome-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!--CSS do Projeto-->
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="<?=$BASE_URL?>css/style.css">
 </head>
 <body>
-    <h1>Testando MovieStar.</h1>
-    <i class="far fa-plus-square"></i>
-
+   <header>
+    <nav id="main-navbar" class="navbar navbar-expand-lg">
+        <a href="<?=$BASE_URL?>" class="navbar-brand">
+            <img src="<?=$BASE_URL?>img/logo.svg" alt="MovieStar" id="logo">
+            <span id="moviestar-title">MovieStar</span>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Togglenavigation">
+            <i class="fas fa-bars"></i>
+        </button>
+        <form action="" id="search-form" class="form-inline my-2 my-lg-0" method="get">
+            <input type="text" name="q" id="search" class="form-control mr-sm-2" type="search" placeholder="Buscar Filmes" aria-label="Search">
+        </form>
+        <button id="magnifier" class="btn my-2 my-sm-0" type="submit">
+            <i class="fas fa-search"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbar">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="<?=$BASE_URL?>auth.php" class="nav-link">Entrar/Cadastrar</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+   </header>
+   <div id="main-container" class="container-fluid">
+        <h1>Corpo do site</h1>
+   </div>
+   <footer id="footer">
+        <div id="social-container">
+            <ul>
+                <li><a href="#"><i class="fab fa-facebook-square"></i></a></li>
+                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+            </ul>
+        </div>
+        <div id="footer-links-container">
+            <ul>
+                <li><a href="#">Adicionar Filme</a></li>
+                <li><a href="#">Adicionar Crítica</a></li>
+                <li><a href="#">Entrar/Registrar</a></li>
+            </ul>
+        </div>
+        <p>&copy; 2023 Contato12</p>
+   </footer>
     <!--JQuery-->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <!--Bootstrap JS-->
