fe65fbc4c8f8a275c151fbf99c7eeb3619f20699
contato12 <contato12araujo@gmail.com>
2023-01-12 22:41:33 -0300

    Created templates.

---
+++ templates/footer.php
@@ -0,0 +1,23 @@
+        <footer id="footer">
+                <div id="social-container">
+                    <ul>
+                        <li><a href="#"><i class="fab fa-facebook-square"></i></a></li>
+                        <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                        <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+                    </ul>
+                </div>
+                <div id="footer-links-container">
+                    <ul>
+                        <li><a href="#">Adicionar Filme</a></li>
+                        <li><a href="#">Adicionar Crítica</a></li>
+                        <li><a href="<?=$BASE_URL?>auth.php">Entrar/Registrar</a></li>
+                    </ul>
+                </div>
+                <p>&copy; 2023 Contato12</p>
+        </footer>
+        <!--JQuery-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <!--Bootstrap JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.3/js/bootstrap.js" integrity="sha512-L6XANV6sOsx9N9c787eDN1pjB2Pzautd3xDgn4cMKuoleHSuCJi5pCDGPCtwE3Bd4A1Olnr0k0aQXbczYzg+wg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    </body>
+</html>
